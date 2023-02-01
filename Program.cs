94f4976433ef2aadebe75b2395571aba9a62e3ba
Alex Isaak <gafar@xaker.ru>
2023-02-01 20:23:21 +0300

    Itogovy script

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+ 
+namespace vtoraya_cifra
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите трехзначное число: ");
+            int chislo = Convert.ToInt32(Console.ReadLine());
+            string chislo_string = Convert.ToString(chislo);
+            Console.WriteLine("Вторая цифра этого числа: " + chislo_string[1]);
+        }
+    }
+}
+
+
